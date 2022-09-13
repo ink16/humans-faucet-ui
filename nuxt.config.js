9f63f83340201df88bf37dfbb52db6bb3f5b3351
ink16 <rares@jekyo.com>
2022-09-13 12:34:09 +0300

    fix: reCAPTCHA site key

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
     [
       '@nuxtjs/recaptcha', {
         //siteKey: '6Lfvc08dAAAAAFYxguVoURPuBLzt6wXccJLmltI8', humans key
-        siteKey: '6Ld5h_QhAAAAAJWXjrDttD5Qa6V1bWvcHaSCsQ6x', //vercel key
+        siteKey: '6Ld5h_QhAAAAAAwgRiOZp_eK6-Ij6Y9QMsoBdKje', //vercel key
         version: 'v2'
       },
     ]
